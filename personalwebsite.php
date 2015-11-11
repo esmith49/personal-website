97570bfe61c0c404b9bf858c2fc19d7cbd3f8a37
Evan Smith <esmith49@cnm.edu>
2015-11-11 14:55:56 -0700

    setting up personal website

---
+++ personalwebsite.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>WebSmithDesigns</title>
+	</head>
+	<body>
+		<h1></h1>
+		<p></p>
+		<h2></h2>
+		<p></p>
+	</body>
+</html>
