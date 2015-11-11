69fdb0174c946220a8a1ae358b5af48b43f822a3
Evan Smith <esmith49@cnm.edu>
2015-11-11 16:34:53 -0700

    setting up personal website

--- personal-website.php
+++ personal-website.php
@@ -5,9 +5,11 @@
 		<title>WebSmithDesigns</title>
 	</head>
 	<body>
-		<h1>Headline goes here</h1>
-		<p></p>
+		<h1>Purpose of WebSmithDesigns.com</h1>
+		<p>For my personal website I want to create a platform that will allow me to market my company offering the services of website design and social media services for small to medium sized business that would not otherwise use these effictive marketing tools</p>
 		<h2></h2>
-		<p></p>
+		<p>The specific audience for this website would be small to medium sized businesses owned by people between the ages of 40 and 70 that may not be able, aware or may not want to use the internet and social media to promote thier businesses</p>
+
+
 	</body>
 </html>
