c77f60d7d535ce5e44ec2be8cea85ae58259e1df
Evan Smith <esmith49@cnm.edu>
2015-12-02 13:14:24 -0700

    updating milestone 2

---
+++ public_html/Documentation/milwstone-2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>WebSmithDesigns</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<br></br>
+		<p><ul>Name: Maria Gonzalez</ul>
+			<ul>Age: 45</ul>
+
+			<ul>Profession: Owner of a small resturant in Albuquerque. The resturant makes the worlds best empanadas and has good 			foot traffic and word of mouth
+			advertising within the small local neighborhood community.</ul>
+
+			<ul>Technology: Maria is a windows user that uses the computer to manage her books and send e-mails to her kids and
+			grandkids,
+			she also has a Iphone 5s that she uses mostly for making calls and utilizing the timer function for an alarm clock and
+			a timer for cooking times but not much else.</ul>
+
+			<ul>Attitudes and Behaviors: Maria is the best cook in the Albuquerque and loves to feed people but she always makes 			more that she needs for the day
+			she would like to get more people to come into her resturant but has found that radio and coupon advertisement does not
+			work and costs her more money than it is worth. Maria does not have time to get more people to the resturant because
+			she is only focused on making the best food.</ul>
+
+			<ul>Frustrations and Needs: Maria wants more customers to know about her resturant but does not want to dealDavid
+			with trying to figure out the best advertiseing options and hates paying for things that do not work.  Maria needs to
+			increase
+			visability without having to spend extra time that she does not have.</ul>
+
+			<ul>Goals: Maria wants to expand the business beyond the local neighborhood customers and try to get some of the tourist
+			dollars
+			that flow into the city during events like the Baloon Fiesta etc...</ul>
+
+		</p>
+		<br></br>
+		<h2>Use Case</h2>
+		<br></br>
+		<p>Maria Gonzalez hears about my website from another restaurant that has used my services and had sucess
+			she will go home and look at my one page and decide to give it a try because there is very little required on her part
+			to accomplish her goals. Maris is viewing the site on her windows 10 enabled computer at home.  Maria will look at
+			the testimonials and some of the other projects that we are currently working on and maintaining.
+			Maria will send me an email explaining what she would like and I will head down to her restaurant to try
+			the empanadas for research purposes.
+
+			I can explain to Maria the benifits of having a website with her menu description of her resturant on it.
+			I will show her how having profiles on Faceboook, Twitter, Yelp and TripAdvisor can help her business.</p>
+<img src="../Images/screenshot-1.png">
+
+
+	</body>
+</html>
---
+++ public_html/Documentation/personal-website.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>WebSmithDesigns</title>
+	</head>
+	<body>
+		<h1>Purpose of WebSmithDesigns.com</h1>
+		<p>For my personal website I want to create a platform that will allow me to market my company offering the services of website design and social media services for small to medium sized business that would not otherwise use these effictive marketing tools</p>
+		<h2></h2>
+		<p>The specific audience for this website would be small to medium sized businesses owned by people between the ages of 40 and 70 that may not be able, aware or may not want to use the internet and social media to promote thier businesses</p>
+
+
+	</body>
+</html>
